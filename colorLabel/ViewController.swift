7884138c19e4f580e3f9a64fe9ff60f4aa4980c3
Zalman Zoteev <sz.zoteev@gmail.com>
2023-04-30 22:30:37 +0300

    create backend

--- colorLabel/ViewController.swift
+++ colorLabel/ViewController.swift
@@ -9,11 +9,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+//    MARK: -ABOutlets
+    
+    @IBOutlet var mainView: UIView!
+    
+    @IBOutlet var redLabelValue: UILabel!
+    @IBOutlet var greenLabelValue: UILabel!
+    @IBOutlet var blueLabelValue: UILabel!
+    
+    @IBOutlet var redSliderValue: UISlider!
+    @IBOutlet var greenSliderValue: UISlider!
+    @IBOutlet var blueSliderValue: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redLabelValue.text = String(format: "%.2f", redSliderValue.value)
+        redSliderValue.maximumTrackTintColor = .clear
+        redSliderValue.minimumTrackTintColor = .red
+        
+        greenLabelValue.text = String(format: "%.2f", greenSliderValue.value)
+        greenSliderValue.minimumTrackTintColor = .green
+        greenSliderValue.maximumTrackTintColor = .clear
+        
+        blueLabelValue.text = String(format: "%.2f", blueSliderValue.value)
+        blueSliderValue.minimumTrackTintColor = .blue
+        blueSliderValue.maximumTrackTintColor = .clear
+        
+        changeColourView()
+        mainView.layer.cornerRadius = 10
     }
 
+    func changeColourView() {
+        mainView.backgroundColor = UIColor(
+            red: CGFloat(redSliderValue.value),
+            green: CGFloat(greenSliderValue.value),
+            blue: CGFloat(blueSliderValue.value),
+            alpha: 1)}
 
+//    MARK: -ABOActions
+    @IBAction func redSlider() {
+        redLabelValue.text = String(format: "%.2f", redSliderValue.value)
+        changeColourView()
+    }
+    @IBAction func greenSlider() {
+       greenLabelValue.text = String(format: "%.2f",greenSliderValue.value)
+        changeColourView()
+    }
+    @IBAction func blueSlider() {
+        blueLabelValue.text = String(format: "%.2f", blueSliderValue.value)
+        changeColourView()
+    }
 }
-
